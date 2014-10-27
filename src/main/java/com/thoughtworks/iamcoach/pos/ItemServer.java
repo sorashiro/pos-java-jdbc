fdd93860624d9a7d272b6363c545a9551c5a662e
WangXiming <wangximingde@163.com>
2014-10-27 20:04:29 +0800

    update findItem Method

--- src/main/java/com/thoughtworks/iamcoach/pos/ItemServer.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/ItemServer.java
@@ -5,14 +5,16 @@ import java.util.List;
 public class ItemServer {
 
     StorageServer storageServer = new StorageServer();
-
+    PromotionServer promotionServer = new PromotionServer();
     public Item findItem(String barcode) {
         List<Item> items = storageServer.getItems();
 
         Item result = new Item();
         for (Item item : items) {
             if (barcode.equals(item.getBarcode())) {
+                List<Promotion> promotionList = promotionServer.getPromotionByBarcode(barcode);
                 result = item;
+                result.setPromotionList(promotionList);
                 break;
             }
         }
