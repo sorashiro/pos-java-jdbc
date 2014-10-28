b4edf148088250e6712954f70ce3d196d349c560
sorashiro <yufei930101@gmail.com>
2014-10-28 17:54:29 +0800

    add getItemPromotionList method

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/ItemDao.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/ItemDao.java
@@ -2,6 +2,7 @@ package com.thoughtworks.iamcoach.pos.dao;
 
 import com.thoughtworks.iamcoach.pos.util.DatabaseUtil;
 import com.thoughtworks.iamcoach.pos.vo.Item;
+import com.thoughtworks.iamcoach.pos.vo.Promotion;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -43,10 +44,32 @@ public class ItemDao {
             item = new Item(resultSet.getInt("id"), resultSet.getString("barcode"),
                     resultSet.getString("name"), resultSet.getString("unit"),
                     resultSet.getDouble("price"), resultSet.getString("category"));
+            preparedStatement.close();
+            resultSet.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return item;
+    }
+
+    public List<Promotion> getItemPromotionList(int id) {
+        List<Promotion> promotionList = new ArrayList<Promotion>();
+        String sql = "SELECT promotion.* FROM promotion,promotion_item " +
+                "WHERE promotion_item.item_id=? AND promotion.id=promotion_item.promotion_id";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Promotion promotion = new Promotion(resultSet.getInt("id"), resultSet.getString("type"),
+                resultSet.getInt("level"));
+                promotionList.add(promotion);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return promotionList;
     }
 
 
