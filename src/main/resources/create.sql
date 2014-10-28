9ffa0154f3e7dd03fc60d6eacdfb76b58a420faa
WangXiming <wangximingde@163.com>
2014-10-28 14:28:56 +0800

    update create.sql

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -14,19 +14,21 @@ price DOUBLE
 ## 创建优惠信息表
 CREATE TABLE promotion (
   id INT AUTO_INCREMENT PRIMARY KEY ,
-  type VARCHAR(50)
+  type VARCHAR(50),
+  level INT
 );
 
 ## 创建商品_优惠连接表
 CREATE TABLE item_promotion (
   item_id INT ,
-  promotion_id INT
+  promotion_id INT,
+  discount INT 
 );
 
 ## 插入初始数据
 INSERT INTO item (id,barcode,name,unit,price)
 VALUES(000001,'ITEM000001','雪碧','瓶',3.5);
 
-INSERT INTO promotion (id,type)
-    VALUES (0001, 'buy_two_get_one_free');
+INSERT INTO promotion (id,type,level)
+    VALUES (0001, 'buy_two_get_one_free',1);
 COMMIT;
