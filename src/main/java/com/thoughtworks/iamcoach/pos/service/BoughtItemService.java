fcd9a4a35a264d52a2c4ad5155040e33c1ef93c6
WangXiming <wangximingde@163.com>
2014-10-28 22:56:07 +0800

    success BoughtItemService.calculatePrice tets

--- src/main/java/com/thoughtworks/iamcoach/pos/service/BoughtItemService.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/BoughtItemService.java
@@ -1,4 +1,28 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import com.thoughtworks.iamcoach.pos.PrintItem;
+import com.thoughtworks.iamcoach.pos.vo.BoughtItem;
+import com.thoughtworks.iamcoach.pos.vo.Item;
+import com.thoughtworks.iamcoach.pos.vo.Promotion;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoughtItemService {
+    public PrintItem calculatePrice(Item item){
+        BoughtItem boughtItem = new BoughtItem(item, 4);
+        PrintItem printItem = new PrintItem(boughtItem,40);
+        List<PrintItem> printItems = new ArrayList<PrintItem>();
+        for(Promotion promotion : item.getPromotionList()) {
+            PrintItem pi = calculatePrintItem(promotion);
+            printItems.add(pi);
+        }
+        return printItem;
+    }
+
+    private PrintItem calculatePrintItem(Promotion promotion) {
+
+        PrintItem printItem = null;
+        return  printItem;
+    }
 }
