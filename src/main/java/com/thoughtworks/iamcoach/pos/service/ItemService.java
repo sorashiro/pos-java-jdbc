82f7c0b7f06e3d3cc826383917740fd50a5f6032
sorashiro <yufei930101@gmail.com>
2014-10-28 18:12:54 +0800

    delete useless code

--- src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
@@ -9,23 +9,6 @@ import java.util.List;
 public class ItemService {
 
     ItemDao itemDao = new ItemDao();
-    StorageService storageService = new StorageService();
-    PromotionService promotionService = new PromotionService();
-    public Item findItem(String barcode) {
-        List<Item> items = storageService.getItems();
-
-        Item result = new Item();
-        for (Item item : items) {
-            if (barcode.equals(item.getBarcode())) {
-                List<Promotion> promotionList = promotionService.getPromotionByBarcode(barcode);
-                result = item;
-                result.setPromotionList(promotionList);
-                break;
-            }
-        }
-
-        return result;
-    }
 
     public Item findItemById(int id) {
         Item item = itemDao.getItemById(id);
--- src/test/java/com/thoughtworks/iamcoach/pos/serviceTest/ItemServiceTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/serviceTest/ItemServiceTest.java
@@ -8,15 +8,6 @@ import static org.fest.assertions.api.Assertions.assertThat;
 public class ItemServiceTest {
   ItemService itemService = new ItemService();
 
-  @Test
-  public void find_item_test() {
-    String barcode = "ITEM000001";
-    assertThat(itemService.findItem(barcode).getBarcode()).isEqualTo("ITEM000001");
-    assertThat(itemService.findItem(barcode).getName()).isEqualTo("可口可乐");
-    assertThat(itemService.findItem(barcode).getPrice()).isEqualTo(3.50);
-    assertThat(itemService.findItem(barcode).getUnit()).isEqualTo("瓶");
-  }
-
     @Test
     public void find_item_by_id_test() {
         int id = 1;
