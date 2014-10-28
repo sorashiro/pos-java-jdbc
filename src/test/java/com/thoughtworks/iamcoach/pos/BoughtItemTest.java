378d3a061e0bc0c2b8d04bff23182d4421f30594
WangXiming <wangximingde@163.com>
2014-10-28 21:31:37 +0800

    update calculate_price_test

--- src/test/java/com/thoughtworks/iamcoach/pos/BoughtItemTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/BoughtItemTest.java
@@ -2,15 +2,29 @@ package com.thoughtworks.iamcoach.pos;
 
 
 import com.thoughtworks.iamcoach.pos.vo.BoughtItem;
+import com.thoughtworks.iamcoach.pos.vo.Item;
+import com.thoughtworks.iamcoach.pos.vo.Promotion;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
 
 public class BoughtItemTest {
-    private BoughtItem boughtItem = new BoughtItem();
+    private Item item = new Item(1, "ITEM000001", "雪碧", "瓶", 3.50, "饮料");
+    private BoughtItem boughtItem = new BoughtItem(item, 2.00);
+    private List<Promotion> promotionList = new ArrayList<Promotion>();
+    private Promotion buyTwoGetOneFreePromotion = new Promotion(1, "buy_two_get_one_free", 1);
+    private Promotion secondHalfPricePromotion = new Promotion(1, "second_half_price", 2);
+    private Promotion discountPromotion = new Promotion(1, "discount", 3);
+
     @Test
     public void calculate_price_test() {
+        promotionList.add(buyTwoGetOneFreePromotion);
+        promotionList.add(secondHalfPricePromotion);
+        promotionList.add(discountPromotion);
         assertThat(boughtItem.calculatePrice().getSubtoal()).isEqualTo(40.0);
     }
 }
