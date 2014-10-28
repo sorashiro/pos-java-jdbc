5964dc711c8dcceb55c3ff99b9c9f2a8f8d2713f
WangXiming <wangximingde@163.com>
2014-10-28 21:19:50 +0800

    add calculate_price_test

--- src/test/java/com/thoughtworks/iamcoach/pos/BoughtItemTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/BoughtItemTest.java
@@ -1,5 +1,16 @@
 package com.thoughtworks.iamcoach.pos;
 
 
+import com.thoughtworks.iamcoach.pos.vo.BoughtItem;
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+
 public class BoughtItemTest {
+    private BoughtItem boughtItem = new BoughtItem();
+    @Test
+    public void calculate_price_test() {
+        assertThat(boughtItem.calculatePrice().getSubtoal()).isEqualTo(40.0);
+    }
 }
