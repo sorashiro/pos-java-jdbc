d096e761e2b5a473840928b6e6df8e3c9c4fd3d2
sorashiro <yufei930101@gmail.com>
2014-10-28 15:43:16 +0800

    update Item class

--- src/main/java/com/thoughtworks/iamcoach/pos/vo/Item.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/vo/Item.java
@@ -3,6 +3,7 @@ package com.thoughtworks.iamcoach.pos.vo;
 import java.util.List;
 
 public class Item {
+    private int id;
     private String barcode;
     private String name;
     private String unit;
@@ -12,48 +13,33 @@ public class Item {
     public Item() {
     }
 
-
-    public Item(String barcode, String name, String unit, double price) {
-        this.setBarcode(barcode);
-        this.setName(name);
-        this.setUnit(unit);
-        this.setPrice(price);
+    public Item(int id, String barcode, String name, String unit, double price, List<Promotion> promotionList) {
+        this.id = id;
+        this.barcode = barcode;
+        this.name = name;
+        this.unit = unit;
+        this.price = price;
+        this.promotionList = promotionList;
     }
 
-    public void setPromotionList(List<Promotion> promotionList) {
-        this.promotionList = promotionList;
+    public int getId() {
+        return id;
     }
 
     public String getBarcode() {
         return barcode;
     }
 
-    public void setBarcode(String barcode) {
-        this.barcode = barcode;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getUnit() {
         return unit;
     }
 
-    public void setUnit(String unit) {
-        this.unit = unit;
-    }
-
     public double getPrice() {
         return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
     }
 
     public List<Promotion> getPromotionList() {
