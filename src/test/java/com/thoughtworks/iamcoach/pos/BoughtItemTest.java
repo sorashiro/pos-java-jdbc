f2edecd384dc15528b4f899330348da869643031
WangXiming <wangximingde@163.com>
2014-10-28 22:38:46 +0800

    move calucate_price_test to BoughtItemServiceTest

--- src/test/java/com/thoughtworks/iamcoach/pos/BoughtItemTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/BoughtItemTest.java
@@ -13,19 +13,5 @@ import static org.fest.assertions.api.Assertions.assertThat;
 
 
 public class BoughtItemTest {
-    private Item item = new Item(1, "ITEM000001", "雪碧", "瓶", 3.50, "饮料");
-    private List<Promotion> promotionList = new ArrayList<Promotion>();
-    private Promotion buyTwoGetOneFreePromotion = new Promotion(1, "buy_two_get_one_free", 1);
-    private Promotion secondHalfPricePromotion = new Promotion(1, "second_half_price", 2);
-    private Promotion discountPromotion = new Promotion(1, "discount", 3);
 
-    @Test
-    public void calculate_price_test() {
-        promotionList.add(buyTwoGetOneFreePromotion);
-        promotionList.add(secondHalfPricePromotion);
-        promotionList.add(discountPromotion);
-        item.setPromotionList(promotionList);
-        BoughtItem boughtItem = new BoughtItem(item, 2.00);
-        assertThat(boughtItem.calculatePrice().getSubtotal()).isEqualTo(40.0);
-    }
 }
--- src/test/java/com/thoughtworks/iamcoach/pos/serviceTest/BoughtItemServiceTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/serviceTest/BoughtItemServiceTest.java
@@ -1,4 +1,27 @@
 package com.thoughtworks.iamcoach.pos.serviceTest;
 
+import com.thoughtworks.iamcoach.pos.vo.BoughtItem;
+import com.thoughtworks.iamcoach.pos.vo.Item;
+import com.thoughtworks.iamcoach.pos.vo.Promotion;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoughtItemServiceTest {
+    private Item item = new Item(1, "ITEM000001", "雪碧", "瓶", 3.50, "饮料");
+    private List<Promotion> promotionList = new ArrayList<Promotion>();
+    private Promotion buyTwoGetOneFreePromotion = new Promotion(1, "buy_two_get_one_free", 1);
+    private Promotion secondHalfPricePromotion = new Promotion(1, "second_half_price", 2);
+    private Promotion discountPromotion = new Promotion(1, "discount", 3);
+
+    @Test
+    public void calculate_price_test() {
+        promotionList.add(buyTwoGetOneFreePromotion);
+        promotionList.add(secondHalfPricePromotion);
+        promotionList.add(discountPromotion);
+        item.setPromotionList(promotionList);
+        BoughtItem boughtItem = new BoughtItem(item, 2.00);
+        assertThat(boughtItem.calculatePrice().getSubtotal()).isEqualTo(40.0);
+    }
 }
