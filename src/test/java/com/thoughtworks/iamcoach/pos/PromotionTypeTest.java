02a756a4354887a5ef6af1ea656feab78c326bef
sorashiro <yufei930101@gmail.com>
2014-10-29 16:58:16 +0800

    delete code

--- src/test/java/com/thoughtworks/iamcoach/pos/PromotionTypeTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/PromotionTypeTest.java
@@ -1,11 +1,5 @@
 package com.thoughtworks.iamcoach.pos;
 
-import com.thoughtworks.iamcoach.pos.vo.BoughtItem;
-import com.thoughtworks.iamcoach.pos.vo.Item;
-import org.junit.Test;
-
-import static org.fest.assertions.api.Assertions.assertThat;
-
 public class PromotionTypeTest {
   PromotionType promotionType = new PromotionType();
 
