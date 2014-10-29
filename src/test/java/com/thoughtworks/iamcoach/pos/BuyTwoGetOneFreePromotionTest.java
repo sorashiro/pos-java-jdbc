6550539467f29ffc775ea4e2fcb87bde6e649a0b
WangXiming <wangximingde@163.com>
2014-10-29 11:14:04 +0800

    update BuyTwoGetOneFree Test

--- src/test/java/com/thoughtworks/iamcoach/pos/BuyTwoGetOneFreePromotionTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/BuyTwoGetOneFreePromotionTest.java
@@ -13,15 +13,15 @@ public class BuyTwoGetOneFreePromotionTest {
   public void calculate_test() {
     Item item = new Item("ITEM000002", "apple", "kg", 10.00);
     BoughtItem boughtItem = new BoughtItem(item, 5.0);
-    assertThat(buyTwoGetOneFree.calculate(boughtItem).getSubtotal()).isEqualTo(40.0);
+    assertThat(buyTwoGetOneFree.calculate(boughtItem)).isEqualTo(40.0);
 
     BoughtItem boughtItem1 = new BoughtItem(item, 3.0);
-    assertThat(buyTwoGetOneFree.calculate(boughtItem1).getSubtotal()).isEqualTo(20.0);
+    assertThat(buyTwoGetOneFree.calculate(boughtItem1)).isEqualTo(20.0);
 
     BoughtItem boughtItem2 = new BoughtItem(item, 1.0);
-    assertThat(buyTwoGetOneFree.calculate(boughtItem2).getSubtotal()).isEqualTo(10.0);
+    assertThat(buyTwoGetOneFree.calculate(boughtItem2)).isEqualTo(10.0);
 
     BoughtItem boughtItem3 = new BoughtItem(item, 0.0);
-    assertThat(buyTwoGetOneFree.calculate(boughtItem3).getSubtotal()).isEqualTo(0.0);
+    assertThat(buyTwoGetOneFree.calculate(boughtItem3)).isEqualTo(0.0);
   }
 }
