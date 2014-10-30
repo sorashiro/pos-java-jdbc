8e3b2853ee487234770568b29be9f0224cf54d1c
sorashiro <yufei930101@gmail.com>
2014-10-30 17:18:38 +0800

    move getItemPromotionList Method to PromotionDao

--- src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.iamcoach.pos.service;
 
 import com.thoughtworks.iamcoach.pos.dao.ItemDao;
+import com.thoughtworks.iamcoach.pos.dao.PromotionDao;
 import com.thoughtworks.iamcoach.pos.vo.Item;
 
 import java.util.List;
@@ -8,10 +9,11 @@ import java.util.List;
 public class ItemService {
 
     ItemDao itemDao = new ItemDao();
+    PromotionDao promotionDao = new PromotionDao();
 
     public Item findItemById(int id) {
         Item item = itemDao.getItemById(id);
-        item.setPromotionList(itemDao.getItemPromotionList(id));
+        item.setPromotionList(promotionDao.getItemPromotionList(id));
         return item;
     }
 
@@ -21,7 +23,7 @@ public class ItemService {
 
     public Item findItemByBarcode(String barcode) {
         Item item = itemDao.getItemByBarcode(barcode);
-        item.setPromotionList(itemDao.getItemPromotionList(item.getId()));
+        item.setPromotionList(promotionDao.getItemPromotionList(item.getId()));
 
         return item;
     }
