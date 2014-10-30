fbb9468e3d88881e405ee60235a0f49d14915ac1
WangXiming <wangximingde@163.com>
2014-10-30 16:06:09 +0800

    success get_barcode_times_test

--- src/test/java/com/thoughtworks/iamcoach/pos/BarcodeTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/BarcodeTest.java
@@ -9,10 +9,10 @@ import static org.fest.assertions.api.Assertions.assertThat;
 
 public class BarcodeTest {
     private Barcode barcode = new Barcode();
-    List<String> barcodeList = new ArrayList<String>();
 
     @Test
     public void unique_barcodes_test() {
+        List<String> barcodeList = new ArrayList<String>();
         barcodeList.add("ITEM000001");
         barcodeList.add("ITEM000001");
         barcodeList.add("ITEM000002");
@@ -21,6 +21,16 @@ public class BarcodeTest {
         assertThat(uniqueBarcodes.get(0)).isEqualTo("ITEM000001");
         assertThat(uniqueBarcodes.get(1)).isEqualTo("ITEM000002");
         assertThat(uniqueBarcodes.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void get_barcode_times_test() {
+        List<String> barcodeList = new ArrayList<String>();
+        barcodeList.add("ITEM000001");
+        barcodeList.add("ITEM000002");
+        barcodeList.add("ITEM000002");
+        assertThat(barcode.getBarcodeTimes(barcodeList, "ITEM000002")).isEqualTo(2);
+        assertThat(barcode.getBarcodeTimes(barcodeList, "ITEM000001")).isEqualTo(1);
     }
 
 }
