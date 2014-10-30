e2906e3c66d271905b815f0523c7448d27935991
WangXiming <wangximingde@163.com>
2014-10-30 15:56:47 +0800

    success unique_barcodes_test

--- src/test/java/com/thoughtworks/iamcoach/pos/BarcodeTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/BarcodeTest.java
@@ -1,4 +1,26 @@
 package com.thoughtworks.iamcoach.pos;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class BarcodeTest {
+    private Barcode barcode = new Barcode();
+    List<String> barcodeList = new ArrayList<String>();
+
+    @Test
+    public void unique_barcodes_test() {
+        barcodeList.add("ITEM000001");
+        barcodeList.add("ITEM000001");
+        barcodeList.add("ITEM000002");
+
+        List<String> uniqueBarcodes = barcode.uniqueBarcode(barcodeList);
+        assertThat(uniqueBarcodes.get(0)).isEqualTo("ITEM000001");
+        assertThat(uniqueBarcodes.get(1)).isEqualTo("ITEM000002");
+        assertThat(uniqueBarcodes.size()).isEqualTo(2);
+    }
+
 }
