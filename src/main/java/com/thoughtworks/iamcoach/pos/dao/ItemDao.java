ceeb12c805cefca4e093c07e6b2b9e8ab473418c
sorashiro <yufei930101@gmail.com>
2014-10-30 16:42:30 +0800

    add database close code

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/ItemDao.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/ItemDao.java
@@ -10,24 +10,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ItemDao {
-    private Connection connection = DatabaseUtil.getConnection();
+    private Connection connection = null;
+    private Statement statement = null;
+    private ResultSet resultSet = null;
 
     public List<Item> getItems() {
         List<Item> itemList = new ArrayList<Item>();
         String sql = "SELECT * FROM item";
 
         try {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql);
+            connection = DatabaseUtil.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
             while (resultSet.next()) {
                 Item item = new Item(resultSet.getInt("id"), resultSet.getString("barcode"),
                         resultSet.getString("name"), resultSet.getString("unit"),
                         resultSet.getDouble("price"), resultSet.getString("category"));
                 itemList.add(item);
             }
+            resultSet.close();
+            statement.close();
+            DatabaseUtil.closeConnection();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
         return itemList;
     }
 
@@ -35,6 +42,7 @@ public class ItemDao {
         Item item = null;
         String sql = "SELECT * FROM item WHERE id=?";
         try {
+            connection = DatabaseUtil.getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -44,6 +52,7 @@ public class ItemDao {
                     resultSet.getDouble("price"), resultSet.getString("category"));
             preparedStatement.close();
             resultSet.close();
+            DatabaseUtil.closeConnection();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -55,6 +64,7 @@ public class ItemDao {
         String sql = "SELECT promotion.*,promotion_item.discount FROM promotion,promotion_item " +
                 "WHERE promotion_item.item_id=? AND promotion.id=promotion_item.promotion_id";
         try {
+            connection = DatabaseUtil.getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -63,7 +73,9 @@ public class ItemDao {
                         resultSet.getInt("discount"), resultSet.getInt("level"));
                 promotionList.add(promotion);
             }
-
+            resultSet.close();
+            preparedStatement.close();
+            DatabaseUtil.closeConnection();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -74,6 +86,7 @@ public class ItemDao {
         Item item = null;
         String sql = "SELECT * FROM item WHERE barcode=?";
         try {
+            connection = DatabaseUtil.getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setString(1, barcode);
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -81,8 +94,10 @@ public class ItemDao {
             item = new Item(resultSet.getInt("id"), resultSet.getString("barcode"),
                     resultSet.getString("name"), resultSet.getString("unit"),
                     resultSet.getDouble("price"), resultSet.getString("category"));
-            preparedStatement.close();
+
             resultSet.close();
+            preparedStatement.close();
+            DatabaseUtil.closeConnection();
         } catch (SQLException e) {
             e.printStackTrace();
         }
