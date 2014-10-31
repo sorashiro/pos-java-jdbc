32df5a177c410f804cb4f4eca8f2c6d18c981954
sorashiro <yufei930101@gmail.com>
2014-10-31 15:16:59 +0800

    add create_promotion_test

--- src/test/java/com/thoughtworks/iamcoach/pos/PromotionFactoryTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/PromotionFactoryTest.java
@@ -1,6 +1,21 @@
 package com.thoughtworks.iamcoach.pos;
 
-public class PromotionFactoryTest {
-    PromotionFactory promotionFactory = new PromotionFactory();
+import com.thoughtworks.iamcoach.pos.promotion.BuyTwoGetOneFreePromotion;
+import com.thoughtworks.iamcoach.pos.promotion.DiscountPromotion;
+import com.thoughtworks.iamcoach.pos.promotion.SecondHalfPricePromotion;
+import org.junit.Test;
 
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class PromotionFactoryTest {
+
+    @Test
+    public void create_promotion_test() {
+        String type1 = "buy_two_get_one_free";
+        String type2 = "second_half_price";
+        String type3 = "discount";
+        assertThat(PromotionFactory.createPromotion(1, type1, 0, 1)).isInstanceOf(BuyTwoGetOneFreePromotion.class);
+        assertThat(PromotionFactory.createPromotion(1, type2, 0, 1)).isInstanceOf(SecondHalfPricePromotion.class);
+        assertThat(PromotionFactory.createPromotion(1, type3, 75, 1)).isInstanceOf(DiscountPromotion.class);
+    }
 }
