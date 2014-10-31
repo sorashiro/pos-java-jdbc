837b4e59d77927502ef95d701b49f86cacddca51
sorashiro <yufei930101@gmail.com>
2014-10-31 14:41:17 +0800

    update code

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/PromotionDao.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/PromotionDao.java
@@ -17,18 +17,22 @@ public class PromotionDao {
 
     public List<Promotion> getItemPromotionList(int id) {
         List<Promotion> promotionList = new ArrayList<Promotion>();
+
         String sql = "SELECT promotion.*,promotion_item.discount FROM promotion,promotion_item " +
                 "WHERE promotion_item.item_id=? AND promotion.id=promotion_item.promotion_id";
+
         try {
             connection = DatabaseUtil.getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
+
             while (resultSet.next()) {
-                Promotion promotion = PromotionFactory.createPromotion(resultSet.getInt("id"), resultSet.getString("type"),
-                        resultSet.getInt("discount"), resultSet.getInt("level"));
+                Promotion promotion = PromotionFactory.createPromotion(resultSet.getInt("id"),
+                        resultSet.getString("type"),resultSet.getInt("discount"), resultSet.getInt("level"));
                 promotionList.add(promotion);
             }
+
             resultSet.close();
             preparedStatement.close();
             DatabaseUtil.closeConnection();
